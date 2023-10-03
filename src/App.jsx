7456b4bb471f9c96d8ad90bde6e2f87425f340dc
venmorr <stevenmorrrca@email.com>
2023-10-03 00:43:54 -0400

    add floorPlan component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import FloorPlan from './floorPlan/FloorPlan.jsx'
+
+
+
+function App() {
+  const rooms = 0
+
+  return (
+    <>
+      <FloorPlan rooms={rooms}/>
+    </>
+  )
+}
+
+export default App
